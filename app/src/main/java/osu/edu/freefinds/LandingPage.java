798020eb348b3f1c4057cdc84112256f7730f222
Liz Heym <heym.4@osu.edu>
2017-10-04 15:09:50 -0400

    Added logging to the new landing page

--- app/src/main/java/osu/edu/freefinds/LandingPage.java
+++ app/src/main/java/osu/edu/freefinds/LandingPage.java
@@ -5,9 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 public class LandingPage extends AppCompatActivity {
+
+    public static final String TAG = "LandingPage";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +27,37 @@ public class LandingPage extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Log.d(TAG, "onCreate() called");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 }
