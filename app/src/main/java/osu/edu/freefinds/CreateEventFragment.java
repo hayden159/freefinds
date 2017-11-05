36318f3dfad884f83dd08f98e05390f33161b9da
Liz Heym <heym.4@osu.edu>
2017-11-05 14:23:48 -0500

    Added Facebook logout button and made it so that you can only create an event if you are logged in.

--- app/src/main/java/osu/edu/freefinds/CreateEventFragment.java
+++ app/src/main/java/osu/edu/freefinds/CreateEventFragment.java
@@ -2,6 +2,7 @@ package osu.edu.freefinds;
 
 import android.*;
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -22,6 +23,9 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
@@ -32,6 +36,7 @@ import java.util.Date;
 import java.util.Random;
 
 import static android.app.Activity.RESULT_OK;
+import static com.facebook.FacebookSdk.getApplicationContext;
 import static java.lang.Integer.parseInt;
 
 /**
@@ -124,24 +129,33 @@ public class CreateEventFragment extends Fragment {
             @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View view) {
-                String titleText = titleField.getText().toString();
-                String descriptionText = descriptionField.getText().toString();
-                String location = locationField.getText().toString();
-                int difficulty = difficultyVal.getProgress();
+                if(isLoggedIn()) {
+                    String titleText = titleField.getText().toString();
+                    String descriptionText = descriptionField.getText().toString();
+                    String location = locationField.getText().toString();
+                    int difficulty = difficultyVal.getProgress();
 
-                myEvent.setTitle(titleText);
-                myEvent.setDescription(descriptionText);
-                myEvent.setOsuLocation(location);
-                myEvent.setDifficulty(difficulty);
-                myEvent.setImageFileName(imageFileName);
+                    myEvent.setTitle(titleText);
+                    myEvent.setDescription(descriptionText);
+                    myEvent.setOsuLocation(location);
+                    myEvent.setDifficulty(difficulty);
+                    myEvent.setImageFileName(imageFileName);
 
-                DatabaseReference events = database.child("events");
-                DatabaseReference newEvent = events.push();
+                    DatabaseReference events = database.child("events");
+                    DatabaseReference newEvent = events.push();
 
-                newEvent.setValue(myEvent);
+                    newEvent.setValue(myEvent);
 
-                //leave create event view now that the event is created
-                startActivity(new Intent(getActivity().getApplicationContext(), MainActivity.class));
+                    //leave create event view now that the event is created
+                    startActivity(new Intent(getActivity().getApplicationContext(), MainActivity.class));
+                }else{
+                    Context context = getApplicationContext();
+                    CharSequence text = "Must be logged in to create an event.";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
             }
         });
 
@@ -220,5 +234,10 @@ public class CreateEventFragment extends Fragment {
             text[i] = characters.charAt(rng.nextInt(characters.length()));
         }
         return new String(text);
+    }
+
+    public boolean isLoggedIn() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        return accessToken != null;
     }
 }
--- app/src/main/java/osu/edu/freefinds/MenuFragment.java
+++ app/src/main/java/osu/edu/freefinds/MenuFragment.java
@@ -1,5 +1,6 @@
 package osu.edu.freefinds;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -11,6 +12,11 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.facebook.login.LoginManager;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 
 /**
@@ -41,6 +47,8 @@ public class MenuFragment extends Fragment implements View.OnClickListener  {
         preferences.setOnClickListener(this);
         Button login = (Button) v.findViewById(R.id.button6);
         login.setOnClickListener(this);
+        Button logout = (Button) v.findViewById(R.id.button7);
+        logout.setOnClickListener(this);
 
         return v;
     }
@@ -59,6 +67,15 @@ public class MenuFragment extends Fragment implements View.OnClickListener  {
                 break;
             case R.id.button4:
                 startActivity(new Intent(getActivity().getApplicationContext(), ViewSingleEventActivity.class));
+                break;
+            case R.id.button7:
+                LoginManager.getInstance().logOut();
+                Context context = getApplicationContext();
+                CharSequence text = "Logged out";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
                 break;
         }
     }
